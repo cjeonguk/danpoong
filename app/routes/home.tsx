2410593c887c035a79cac5cdba426e01c76de796
jigangjon <jigangjon@gmail.com>
2026-01-10 22:22:26 +0900

    added groups scroll in index page with embla carousel

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router";
+import useEmblaCarousel from "embla-carousel-react";
 
 export default function Home() {
+  const [emblaGroupsRef] = useEmblaCarousel({
+    dragFree: true,
+  });
   const groupItems = [
     { imgUrl: "https://picsum.photos/200/300", link: "/groups/picsum1" },
     { imgUrl: "https://picsum.photos/200/400", link: "/groups/picsum2" },
@@ -10,30 +14,32 @@ export default function Home() {
   const thisWeek = [6, 7, 8, 9, 10, 11, 12];
   return (
     <div className="flex flex-col gap-6">
-      <div className="flex gap-3">
-        <Link className="shrink-0" to="/profile">
-          <img
-            className="w-[88px] rounded-full aspect-square shadow-md"
-            src="/profile.svg"
-            alt="profile-icon"
-          />
-        </Link>
-        <Link className="shrink-0" to="/groups">
-          <img
-            className="w-[88px] rounded-full aspect-square shadow-md"
-            src="/groups.svg"
-            alt="group-icon"
-          />
-        </Link>
-        {groupItems.map((item, index) => (
-          <Link className="shrink-0" key={index} to={item.link}>
+      <div ref={emblaGroupsRef}>
+        <div className="flex gap-3">
+          <Link className="shrink-0" to="/profile">
             <img
               className="w-[88px] rounded-full aspect-square shadow-md"
-              src={item.imgUrl}
-              alt={`nav-item-${index}`}
+              src="/profile.svg"
+              alt="profile-icon"
             />
           </Link>
-        ))}
+          <Link className="shrink-0" to="/groups">
+            <img
+              className="w-[88px] rounded-full aspect-square shadow-md"
+              src="/groups.svg"
+              alt="group-icon"
+            />
+          </Link>
+          {groupItems.map((item, index) => (
+            <Link className="shrink-0" key={index} to={item.link}>
+              <img
+                className="w-[88px] rounded-full aspect-square shadow-md"
+                src={item.imgUrl}
+                alt={`nav-item-${index}`}
+              />
+            </Link>
+          ))}
+        </div>
       </div>
       <div className="flex flex-col gap-3">
         <Link
